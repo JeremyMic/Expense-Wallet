baf4210dd93d8fb542ddeff354f8cdc2571a4064
JeremyMic <jeremy.yuwono@binus.ac.id>
2023-01-16 08:27:11 +0700

    Add File

---
+++ ExpenseWallet/app/src/main/java/com/example/expensewallet/Record.java
@@ -0,0 +1,47 @@
+package com.example.expensewallet;
+
+public class Record {
+    private int id;
+    private String name;
+    private int amount;
+    private String type;
+
+    public Record(int id,String name, int amount, String type) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
